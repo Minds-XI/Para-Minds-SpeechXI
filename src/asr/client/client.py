e79985117c0dd426f82a55c94d799c7bc4f30389
Ahmed Elsayed <ahmed.elsayed@mindsxi.com>
2025-07-30 09:39:43 +0000

    [Feat]: integrate with WebRTC VAD

--- src/asr/client/client.py
+++ src/asr/client/client.py
@@ -1,23 +1,26 @@
 import os
+import numpy as np
 import pvporcupine
 import pyaudio
 import grpc
 from asr.grpc_generated.asr_pb2 import AudioStream
 from asr.grpc_generated.asr_pb2_grpc import AsrServiceStub
 from dotenv import load_dotenv
+from asr.utils.audio import Frame
+from asr.vad.wbtrc import WebRTCVAD
+from scipy.io import wavfile
 load_dotenv()
 class AudioStreamClient: 
-    ACCESS_KEY = os.getenv("PORCUPINE_API_KEY")
-    MODEL_PATH = "/mnt/d/Minds-Work/asr-service/models/Hey-Minds_en_linux_v3_0_0.ppn"
     def __init__(self):
-        self.porcupine = pvporcupine.create(access_key=self.ACCESS_KEY,
-                                keyword_paths=[self.MODEL_PATH],
-                                sensitivities=[0.5])
-
-        self.frame_len = self.porcupine.frame_length
-        self.sample_rate = self.porcupine.sample_rate
         self.pa = pyaudio.PyAudio()
+        self.frame_len = 30
+        self.sample_rate = 16000
         self.counter_id = 0
+        self.vad_service = WebRTCVAD(
+                                sample_rate=self.sample_rate,
+                                length=self.frame_len,
+                                padding_duration_ms=100
+                                )
     def generator(self):
 
         audio_stream = self.pa.open(format=pyaudio.paInt16,
@@ -26,19 +29,37 @@ class AudioStreamClient:
                                 input=True,
                                 frames_per_buffer=self.frame_len
                             )
-        while True:
-            pcm_bytes = audio_stream.read(self.frame_len, exception_on_overflow=False)
-            yield AudioStream(audio=pcm_bytes,
-                        id=self.counter_id)    
-            self.counter_id += 1
-        
+        speech_segments = []
+        try:
+            while True:
+                pcm_bytes = audio_stream.read(self.frame_len, exception_on_overflow=False)
+                frame_obj = Frame(bytes=pcm_bytes,timestamp=None,duration=None)
+                for result in self.vad_service.process_stream(audio=frame_obj):
+                    if result:
+                        speech_segments.append(result)
+                # yield AudioStream(audio=pcm_bytes,
+            #             id=self.counter_id)    
+            # self.counter_id += 1
+        except KeyboardInterrupt:
+            print("Recording stopped. Saving output_audio.wav")
+            if speech_segments:
+                speechs = [
+                    np.frombuffer(speech, dtype=np.int16).astype(np.float32) / 32768.0
+                    for speech in speech_segments
+                ]
+                speechs = np.concatenate(speechs)
+                wavfile.write('output_audio.wav', self.sample_rate, speechs)
+
+
 def main():
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = AsrServiceStub(channel)
+    # channel = grpc.insecure_channel('localhost:50051')
+    # stub = AsrServiceStub(channel)
     stream = AudioStreamClient()
-    response_iter = stub.processAudio(stream.generator())
-    for response in response_iter:
-        print(response)
+    # response_iter = stub.processAudio(stream.generator())
+    # for response in response_iter:
+    #     print(response)
+    stream.generator()
 
 if __name__== "__main__":
     main()
+
