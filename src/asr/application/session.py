7034b22b18bb5615cd9f6c268c011959b53025ef
Ahmed Elsayed <ahmed.elsayed@mindsxi.com>
2025-08-18 16:16:57 +0000

    [Feat]: add session manager to handle multiple connections and different contexts

---
+++ src/asr/application/session.py
@@ -0,0 +1,28 @@
+import asyncio
+from typing import Union
+from asr.domain.entities import TextChunk
+from asr.transport.grpc.generated.whisper_pb2 import AudioChunk
+
+from asr.core.whisper.processor import OnlineASRProcessor
+
+
+class ConnectionManager:
+    def __init__(self, session_id:int, asr_processor: OnlineASRProcessor):
+        self.session_id = session_id
+        self.audio_queue = asyncio.Queue(maxsize=10)
+        self.text_queue  = asyncio.Queue(maxsize=10)
+        self.asr_processor = asr_processor
+        self.last_end = None
+        self.is_first = True
+
+    async def write_to_audio_queue(self, item:Union[AudioChunk,object]):  
+        await self.audio_queue.put(item)
+
+    async def read_from_audio_queue(self):
+        return await self.audio_queue.get()
+
+    async def write_to_text_queue(self, item:Union[TextChunk,object]):  
+        await self.text_queue.put(item)
+
+    async def read_from_text_queue(self):
+        return await self.text_queue.get()
