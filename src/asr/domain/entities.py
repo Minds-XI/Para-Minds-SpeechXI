8a9ad93a84d703e03f9e332fd3034b9482aa14d2
Ahmed Elsayed <ahmed.elsayed@mindsxi.com>
2025-10-06 12:46:45 +0000

    [Feat]: add main schemas for asr module

--- src/asr/domain/entities.py
+++ src/asr/domain/entities.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+import numpy as np
 from pydantic import BaseModel
 from typing import Optional
 class TextChunk(BaseModel):
@@ -5,4 +7,18 @@ class TextChunk(BaseModel):
     end: Optional[float] = None
     sentence: str = ""
 
+@dataclass
+class AudioChunkDTO:
+    audio_data:np.ndarray
 
+@dataclass
+class ASRResponse:
+    word:str
+    start:float
+    end: float
+
+@dataclass
+class ASRProcessorResponse:
+    sentense:str
+    start:float
+    end:float
