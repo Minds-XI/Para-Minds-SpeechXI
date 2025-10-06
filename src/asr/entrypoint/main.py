a4906a033853c98d34f0d27f089abc56e511a21d
Ahmed Elsayed <ahmed.elsayed@mindsxi.com>
2025-10-06 19:46:23 +0000

    [Refactor]: integrate with refactored code

--- src/asr/entrypoint/main.py
+++ src/asr/entrypoint/main.py
@@ -1,44 +1,20 @@
 import argparse, asyncio
-import os
-import grpc
+
 from loguru import logger
-from asr.core.whisper.utils import add_shared_args
-from shared.protos_gen import whisper_pb2_grpc
-from asr.transport.grpc.service import AsrService
-from confluent_kafka import Consumer,Producer
-from shared.protos_gen.whisper_pb2 import StreamingResponse,AudioChunk
-from confluent_kafka.schema_registry.protobuf import ProtobufSerializer, ProtobufDeserializer
-from confluent_kafka.schema_registry import SchemaRegistryClient
+from asr.infrastructure.whisper.utils import add_shared_args
+from asr.application.commands.asr_service import AsrService
+from asr.domain.entities import KafkaConfig
+from asr.infrastructure.message.audio_sub import KafkaAudioSub
+from asr.infrastructure.message.text_pub import KafkaTextPub
 from dotenv import load_dotenv
 load_dotenv()
-EXTERNAL_HOST = os.environ.get("EXTERNAL_HOST")
-SCHEMA_REGISTRY_PORT = os.environ.get("SCHEMA_REGISTRY_PORT")
-KAFKA_EXTERNAL_PORT = os.environ.get("KAFKA_EXTERNAL_PORT")
-SCHEMA_REGISTRY_URL = f"http://{EXTERNAL_HOST}:{SCHEMA_REGISTRY_PORT}"
-# Schema Registry client
-sr = SchemaRegistryClient({"url": SCHEMA_REGISTRY_URL})
-
-response_serializer = ProtobufSerializer(StreamingResponse, sr)
-response_deserializer = ProtobufDeserializer(StreamingResponse)
-
-req_deserializer = ProtobufDeserializer(AudioChunk)
-
-# Create a consumer configuration
-raw_audio_consumer_config = {
-    'bootstrap.servers': f'{EXTERNAL_HOST}:{KAFKA_EXTERNAL_PORT}',  # Kafka server address
-    'group.id': 'asr-group',                                        # Consumer group name
-    'auto.offset.reset': 'earliest'                                 # Start reading from beginning
-}
-raw_audio_consumer = Consumer(raw_audio_consumer_config)
-raw_audio_consumer.subscribe(['raw-audio'])
-text_producer = Producer({
-        "bootstrap.servers": f"{EXTERNAL_HOST}:{KAFKA_EXTERNAL_PORT}",
-        # "acks": "0",
-        # "enable.idempotence": True,
-        "linger.ms": 5,
-        })
 
 async def serve():
+    kafka_config = KafkaConfig()
+    audio_sub = KafkaAudioSub(config=kafka_config,
+                              topic_name="raw_audio")
+    text_pub = KafkaTextPub(config=kafka_config,
+                            topic_name="text-chunk")
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default=8080)
@@ -46,14 +22,19 @@ async def serve():
     add_shared_args(parser)  # from whisper_online
     args = parser.parse_args()
     asr_service = AsrService(args,
-               raw_audio_consumer=raw_audio_consumer,
-               raw_audio_deserializer=req_deserializer,
-               req_topic_name="raw-audio",
-               text_producer=text_producer,
-               text_producer_topic="text-chunk",
-               response_serializer=response_serializer)
-   
-    await asr_service.StreamingRecognize()
+               audio_consumer=audio_sub,
+               text_producer=text_pub,
+            )
+    await text_pub.start()
+    await audio_sub.start()
+    logger.info("ASR service started.")
+
+    try:
+        await asr_service.stream_recognize()
+    except KeyboardInterrupt:
+        logger.info("KeyboardInterrupt received.")
+    finally:
+        logger.info("ASR service stopped.")
 
 if __name__ == "__main__":
     try:
