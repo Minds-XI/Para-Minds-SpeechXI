65008761f01ffab3f2df16cc74ed141329a97285
Ahmed Elsayed <ahmed.elsayed@mindsxi.com>
2025-10-06 16:49:04 +0000

    [Feat]: add kafka configuration and fix typo

--- src/asr/domain/entities.py
+++ src/asr/domain/entities.py
@@ -1,15 +1,23 @@
 from dataclasses import dataclass
+import os
 import numpy as np
 from pydantic import BaseModel
 from typing import Optional
+from dotenv import load_dotenv
+
 class TextChunk(BaseModel):
     begin: Optional[float] = None
     end: Optional[float] = None
     sentence: str = ""
+    session_id:str
 
 @dataclass
 class AudioChunkDTO:
     audio_data:np.ndarray
+    session_id:str
+    seq:int
+    def __len__(self):
+        return len(self.audio_data)
 
 @dataclass
 class ASRResponse:
@@ -19,6 +27,17 @@ class ASRResponse:
 
 @dataclass
 class ASRProcessorResponse:
-    sentense:str
+    sentence:str
     start:float
     end:float
+
+
+@dataclass
+class KafkaConfig:
+    external_host:str = os.environ.get("EXTERNAL_HOST")
+    schema_registry_port:str = os.environ.get("SCHEMA_REGISTRY_PORT")
+    external_port:str = os.environ.get("KAFKA_EXTERNAL_PORT")
+
+    @property
+    def schema_registry_url(self):
+        return f"http://{self.external_host}:{self.schema_registry_port}"
